026d04f1e8386218e9567504f37c8bbfaa65fa81
Favour <ofavourite001@gmail.com>
2023-03-17 02:57:37 -0700

    Corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
+/* Printing a sentencce using puts */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
+/* Printing a sentence using printf */
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
