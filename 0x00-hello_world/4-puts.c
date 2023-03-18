b79030f80569ddb6282f1e77cde6cc2fc0813e20
Favour <ofavourite001@gmail.com>
2023-03-18 16:20:01 +0000

    4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void) /* Printing a sentencce using puts */
+int main(void)
 {
+	/* Printing a sentencce using puts */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
