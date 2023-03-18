6c7851ec5bd1ce37acd3a97c29aac5668c43b149
Favour <ofavourite001@gmail.com>
2023-03-18 16:16:49 +0000

    Add betty style doc to 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-/* Printing a sentencce using puts */
-
-int main(void)
+int main(void) /* Printing a sentencce using puts */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
