8bf9cbe1aaadcd8962a5562cefedb38f4dc876dc
Favour <ofavourite001@gmail.com>
2023-03-21 18:10:38 -0700

    Fix 2-

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
 /**
- * main - calls function
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
  *
- * Return: Always 0
+ * Return: Always void
  */
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
-}
 
 void print_alphabet_x10(void)
 {
