e057691c112014439d5f8f952cbac30df79017ad
Favour <ofavourite001@gmail.com>
2023-03-21 17:42:38 -0700

    0- and 1-

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Author: Oluwatunmibi Favour O.
+ * Desc: Defines all the functions used in the 0x02-functions_nested_loop directory
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
