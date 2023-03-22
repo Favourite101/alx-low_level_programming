b426a2a2433e4c2e9d89bb398cb0620c638045d6
Favour <ofavourite001@gmail.com>
2023-03-22 04:58:11 -0700

    Compulsory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute number
+ *
+ * Return: Absolute value
+ */
+
+int _abs(int f)
+{
+	if (f >= 0)
+	{
+		return (f);
+	}
+	else
+	{
+		return (-f);
+	}
+}
