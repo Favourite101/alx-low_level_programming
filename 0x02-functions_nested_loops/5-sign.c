bda73033150a35a1af3de732aeb1d7707580d85e
Favour <ofavourite001@gmail.com>
2023-03-22 05:07:54 -0700

    Fix 5-

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
+ * @n: parameter to be checked
  *
  * Return: 1, 0 or -1
  */
@@ -10,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		
+
 		return (1);
 	}
 	else if (n < 0)
