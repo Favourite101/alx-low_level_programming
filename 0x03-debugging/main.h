7fbcc0f83a46a0081ef5f8ca0db8938df80e1ffb
Favour <ofavourite001@gmail.com>
2023-03-23 05:13:24 -0700

    Fix 2--

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/**
+ * File: main.h
+ * Author: Oluwatunmibi Favour O.
+ * Desc: Defines all the functions used in the 0x03-debugging directory
+ */
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
