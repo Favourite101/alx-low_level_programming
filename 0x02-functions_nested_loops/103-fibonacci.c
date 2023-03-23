953e1e97b53cb1a5c60326d62bc5e692f72367bb
Favour <ofavourite001@gmail.com>
2023-03-22 19:29:32 -0700

    Fix 103-

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,22 @@
  */
 int main(void)
 {
-	unsigned long t1 = 1, t2 = 2, sum, totsum;
+	unsigned long t1 = 1, t2 = 2, sum;
+	float totsum;
 
-	for (sum = 0; sum <= 4000000; sum++)
+	while (sum < 4000000)
 	{
-		t1 = t2;
-		t2 = sum;
 		sum =  t1 + t2;
 
-		if (sum % 2 == 0)
+		if ((sum % 2) == 0)
 		{
 			totsum += sum;
 		}
+		t1 = t2;
+		t2 = sum;
+
 	}
-	printf("%lu\n", totsum);
+	printf("%.0f\n", totsum);
 
 	return (0);
 }
