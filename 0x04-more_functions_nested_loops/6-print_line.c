62b2525acb25460dca1d698c0847395acf2d84b1
Favour <ofavourite001@gmail.com>
2023-03-23 20:12:39 -0700

    -4 to -6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		if (n <= 0)
-		{
-			continue;
-		}
-		else
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
