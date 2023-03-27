4eeee6d2ca80a322ca566315a3107e7dcccf2ee3
Favour <ofavourite001@gmail.com>
2023-03-27 07:09:13 -0700

    Fixes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	for (s[i] = '\0'; i < s[i]; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
