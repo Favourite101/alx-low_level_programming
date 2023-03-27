c8ed98e8f75faad68a6be42fc8f5b00ffcced635
Favour <ofavourite001@gmail.com>
2023-03-27 05:02:54 -0700

    Make files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+
+	a = &b;
+	b = &a;
+}
