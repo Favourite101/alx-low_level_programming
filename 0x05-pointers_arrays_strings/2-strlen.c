10e958f04fc1e450860030735cb566087344eac1
Favour <ofavourite001@gmail.com>
2023-03-27 06:48:49 -0700

    2 & 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: number
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(s);
+
+	return(len);
+}
