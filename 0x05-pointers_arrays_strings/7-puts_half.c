12dd847912eed2ebb424c00305642ee7ac09d49f
Favour <ofavourite001@gmail.com>
2023-03-27 21:47:47 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length_of_the_string = 0;
+	while (str[length_of_the_string] != 0)
+	{
+		length_of_the_string++;
+	}
+
+}
