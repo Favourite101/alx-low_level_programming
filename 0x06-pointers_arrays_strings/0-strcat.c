25d820e6192a819551453fef1e6869f42d661f8e
Favour <ofavourite001@gmail.com>
2023-03-30 23:57:30 +0000

    0-

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,14 @@ char *_strcat(char *dest, char *src)
 	int test_1 = 0, test_2 = 0;
 
 	while (dest[test_1] != '\0')
+	{
 		test_1++;
+	}
 	while (src[test_2] != '\0')
 	{
 		dest[test_1] = src[test_2];
-		test_1++;
 		test_2++;
+		test_1++;
 	}
 	dest[test_1] = '\0';
 
