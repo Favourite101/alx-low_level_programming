62856f3e55f0c8fa15097dfcd86e42a839b20426
Favour <ofavourite001@gmail.com>
2023-03-30 21:14:03 +0000

    Fix 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,28 @@
 void puts_half(char *str)
 {
 	int length_of_the_string = 0;
-	while (str[length_of_the_string] != 0)
+	int n;
+
+	while (str[length_of_the_string])
 	{
 		length_of_the_string++;
+	}
+
+	if (n % 2 == 0)
+	{
+		for (n = 0; n <= length_of_the_string / 2; n++)
+		{
+			_putchar(str[n]);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		for (n = 0; n <= (length_of_the_string - 1) / 2)
+		{
+			_putchar(str[n]);
+			_putchar('\n');
+		}
 	}
 
 }
