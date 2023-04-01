a5b50f0b938eff8deaf1b9c56e1863268ce59398
Favour <ofavourite001@gmail.com>
2023-04-01 15:35:30 +0000

    Add 101-

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * print_number - prints an integer
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i = abs(n);
+
+	if (n >= 0 && n < 10)
+	{
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 0 && n > -10)
+	{
+		_putchar('-');
+		_putchar((i % 10) + '0');
+	}
+	else
+	{
+		_putchar(45);
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+	}
+}
