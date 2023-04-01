c9a109c81dc9ee87748a3f6995d97c9ff58ad99f
Favour <ofavourite001@gmail.com>
2023-04-01 19:18:37 +0000

    102-

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,7 +16,7 @@ void print_number(int n)
 		_putchar(45);
 		if (n / 10)
 		{
-			print_number( i / 10);
+			print_number(i / 10);
 		}
 		_putchar((i % 10) + '0');
 
@@ -27,6 +27,6 @@ void print_number(int n)
 		{
 			print_number(n / 10);
 		}
-		_putchar((n % 10) +'0');
+		_putchar((n % 10) + '0');
 	}
 }
