3a3f56b51566d7e661f147519636b1b3fc0c6b7a
Favour <ofavourite001@gmail.com>
2023-04-01 21:13:30 +0000

    103-

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that will store the result
+ * @size_r: size of r
+ *
+ * Return: r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	r = n1 + n2;
+	if (r > n - 1)
+		return (0);
+	else
+		return (r);
+}
