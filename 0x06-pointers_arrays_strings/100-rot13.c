66710a47e0f7b35491c4912db9c2f0d9bb113129
Favour <ofavourite001@gmail.com>
2023-04-01 15:07:48 +0000

    Fix 100-

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,15 +10,15 @@ char *rot13(char *s)
 {
 	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	int i, j = 0;
+	int i, j, k = i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == a[j] && a[j] != b[j])
+			if (s[i] == a[j])
 			{
-				s[i] = b[j];
+				s[k] = b[j];
 			}
 		}
 	}
