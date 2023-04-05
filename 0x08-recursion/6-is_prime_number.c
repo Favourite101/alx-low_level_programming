4255ed56a7d7e733320949d40581a53aad32b1cb
Favour <ofavourite001@gmail.com>
2023-04-05 22:53:46 +0000

    Add 6-

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 for prime numbers
+ * @n: number
+ *
+ * Return 1 or 0;
+ */
+int is_prime_number(int n)
+{
+	if (n < 0 || n == 0 || n == 1)
+	{
+		return (0);
+	}
+}
