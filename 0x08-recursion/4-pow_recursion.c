dc5178cb89ceb5d779b6967edab819a1726221cd
Favour <ofavourite001@gmail.com>
2023-04-05 21:15:20 +0000

    Add 4-

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises a number to a power
+ * @x: number
+ * @y: power
+ *
+ * Return: value
+ */
+int _pow_recursion(int x, int y)
+{
+	int power;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if ( y == 0 || y == 1)
+	{
+		return (1);
+	}
+	if (y > 0)
+	{
+		power = x * _pow_recursion(x, y--);
+	}
+
+	return (power);
+}
