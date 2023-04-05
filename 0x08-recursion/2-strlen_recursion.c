6a60f8b3a698e45e438737deae7aeabddddb3903
Favour <ofavourite001@gmail.com>
2023-04-05 20:31:49 +0000

    Fix 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	if (*s == '\0')
+	{
+		return (0);
+	}
 
-	_strlen_recursion(s + 1);
-	i++;
-
-	return (i);
+	return (_strlen_recursion(s + 1));
 }
