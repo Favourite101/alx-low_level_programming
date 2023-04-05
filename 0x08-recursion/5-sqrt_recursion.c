85ac75f0a6bbb67649a94a557e656d5c0b88a0ea
Favour <ofavourite001@gmail.com>
2023-04-05 22:37:43 +0000

    Fix 5-

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,15 +8,22 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i = n;
+	int i;
 
 	if (n < 0)
 		return (-1);
-	if (i > 0)
+	if (n == 0)
 	{
-		if (i * i == n)
-			return (i);
-		_sqrt_recursion(n - 1);
+		return (0);
 	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	i = n;
+	if (i * i == n)
+		return (i);
+	_sqrt_recursion(i - 1);
+
 	return (-1);
 }
