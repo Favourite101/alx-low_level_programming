961ae0750959c5f143c74c80f375938b09e6ee16
Favour <ofavourite001@gmail.com>
2023-04-05 15:49:18 +0000

    Fix 0-

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
-			_putchar(s[i]);
-			_puts_recursion(s[i + 1]);
+		_putchar(s[i]);
+		_puts_recursion(s + 1);
 	}
 }
