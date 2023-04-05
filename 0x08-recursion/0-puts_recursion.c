448562b2a0b46ad2750438694b8d99657139596e
Favour <ofavourite001@gmail.com>
2023-04-05 15:31:47 +0000

    Add 0-

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "maain.h"
+
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+			_putchar(s[i]);
+			_puts_recursion(s[i + 1]);
+	}
+}
