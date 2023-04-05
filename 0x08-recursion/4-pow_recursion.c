ebdde68bb1dd7e967a2c56943e1040192edaf9f7
Favour <ofavourite001@gmail.com>
2023-04-05 21:43:30 +0000

    Fix 4-

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -19,11 +19,7 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	if (y > 1)
-	{
-		power = x * _pow_recursion(x, y);
-		y--;
-	}
+	power = x * x * _pow_recursion(x, y - 1);
 
 	return (power);
 }
