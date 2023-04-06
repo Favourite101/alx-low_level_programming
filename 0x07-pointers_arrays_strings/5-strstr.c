a744e369dec9502bdc636e534d36d236df9a325d
Favour <ofavourite001@gmail.com>
2023-04-06 15:24:06 +0000

    Fix 5-

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-	
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 			if (haystack[i + j] != needle[j])
 				break;
 		}
-		if (!needle[j])
+		if (needle[j])
 			return (&haystack[i]);
 	}
 	return (NULL);
