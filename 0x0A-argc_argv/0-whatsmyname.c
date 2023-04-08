35d6617181a5cb347e6963cc111157df0c2bf92e
Favour <ofavourite001@gmail.com>
2023-04-08 15:32:47 +0000

    Fix 0-

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
 
 /**
  * main - a program to print the name of this file
+ * @argc: number of arguments
  * @argv: array of strings
  *
  * Return: 0
  */
-int main(int __attribute__ ((unused)) argc, char** argv)
+int main(int __attribute__((unused)) argc, char** argv)
 {
 	printf("%s\n", argv[0]);
 
