de4016497faa760a0ae57e7eefed76b2029825e4
Favour <ofavourite001@gmail.com>
2023-04-08 23:21:04 +0000

    Fix 3-

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,13 @@
 int main(int argc, char **argv)
 {
 	int prod;
-	
+
 	if (argc == 1 || argc == 2)
+	{
 		printf("Error\n");
+	}
 	else
-	{	
+	{
 		prod = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", prod);
 	}
