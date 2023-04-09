bba059fadcdd36d1737fd9a8a5b8576d235ac2a3
Favour <ofavourite001@gmail.com>
2023-04-09 09:47:02 +0000

    Fix 100-

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,64 +1,51 @@
 #include "main.h"
 
-char *reverse_string(char *s, int length, int i);
-int get_length(char *s, int i);
+int get_length(char *s);
+int check_pal(char *s, int j, int length);
 
 /**
  * is_palindrome - checks if a word is a palindrome
  * @s: pointer to string
- *  Return: 0 or 1
+ *
+ * Return: 0 or 1
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int length = get_length(s, i);
-	char *rev = reverse_string(s, length, i);
-
-	if (*s == *rev)
-	{
-		rev++;
-		is_palindrome(s++);
+	if (*s == '\0')
 		return (1);
-	}
-
-	return (0);
-}
-
-/**
- * reverse_string - reverses a string
- * @s: string
- * @length: length of string
- * @i: checker
- *
- * Return: reversed string
- */
-char *reverse_string(char *s, int length, int i)
-{
-	char temp;
-
-	if (i < length / 2)
-	{
-		temp = s[i];
-		s[i] = s[length - i];
-		s[length - i] = temp;
-		reverse_string(s, length, i + 1);
-	}
-	return (s);
+	return (check_pal(s, 0, get_length(s)));
 }
 
 /**
  * get_length - get the length of the string
  * @s: string
- * @i: checker
  *
  * Return: length
  */
-int get_length(char *s, int i)
+int get_length(char *s)
 {
-	if (s[i] != '\0')
-	{
-		get_length(s, i + 1);
-	}
+	if (*s == '\0')
+		return (0);
 
-	return (i);
+	return(1 + get_length(s + 1));
+}
+
+/**
+ * check_pal - checks if its a palindrome
+ * @s: string
+ * @j checker
+ * @length: length of string
+ *
+ * Return: 1 or 0
+ */
+int check_pal(char *s, int j, int length)
+{
+	if (*(s + j) != *(s + length - 1))
+	{
+		return (0);
+	}
+	if (j >= length)
+		return (1);
+
+	return (check_pal(s, j + 1, length - 1));
 }
