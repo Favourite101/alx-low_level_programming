25dbd49a3f1fa84f50f91966e26ed5cf0f8d37d6
Favour <ofavourite001@gmail.com>
2023-04-09 22:16:33 +0000

    Add 9-

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (src[len])
+		i++;
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
