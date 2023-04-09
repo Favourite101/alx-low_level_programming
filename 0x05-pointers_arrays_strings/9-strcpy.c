a4ae52210f14f730ce751039ee7a66b0da303639
Favour <ofavourite001@gmail.com>
2023-04-09 22:23:51 +0000

    Add 9-

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0, i;
 
-	while (src[len])
+	while (src[len] != '\0')
 		len++;
 	for (i = 0; i < len; i++)
 		dest[i] = src[i];
+	dest[i] == '\0';
 
 	return (dest);
 }
