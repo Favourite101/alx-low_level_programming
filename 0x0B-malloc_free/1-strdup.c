d04e4465548a9e15cbddd193890e2b38cafba746
Favour <ofavourite001@gmail.com>
2023-04-11 20:42:50 +0000

    Fix 1-

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,6 +17,8 @@ char *_strdup(char *str)
 	while (str[len] != '\0')
 		len++;
 	dup = malloc(sizeof(*str) * len + 1);
+	if (dup == NULL)
+		return (NULL);
 	for (i = 0; i < len; i++)
 	{
 		dup[i] = str[i];
