a3944a8bbf3c8cd35df7c2b6ea4d8801f6b8d4db
Favour <ofavourite001@gmail.com>
2023-04-11 21:40:40 +0000

    Fix 2-

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-	catt = malloc(sizeof(char) * len1 + len2 + 1);
+	catt = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (catt == NULL)
 		return (NULL);
 	if (s1)
