e04e62de2c19e987aba207c53cf050bb41cd2190
Favour <ofavourite001@gmail.com>
2023-04-12 07:03:24 +0000

    Check 2-

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		return (s2);
 	if (s2 == NULL)
 		return (s1);
-	while (s1[len1] != '\0')
+/*	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
@@ -43,5 +43,5 @@ char *str_concat(char *s1, char *s2)
 	}
 	catt[i] = '\0';
 
-	return (catt);
+	return (catt);*/
 }
