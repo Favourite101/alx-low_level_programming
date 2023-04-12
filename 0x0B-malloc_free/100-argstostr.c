4e1dfe6bbe0190a0b3ef6ace6e9ffefd35b2774f
Favour <ofavourite001@gmail.com>
2023-04-12 20:03:05 +0000

    Add 100-

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments
+ * @ac: number of arguments
+ * @av: array of strings
+ * 
+ * Return: pointer to string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, counter;
+	char *favour;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			counter++;
+		}
+		counter++;
+	}
+	favour = malloc(sizeof(char) * counter);
+	if (favour == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			favour[k] = av[i][j];
+			k++;
+		}
+		favour[k] = '\n';
+		k++;
+	}
+	favour[k] = '\0';
+
+	return(favour);
+}
