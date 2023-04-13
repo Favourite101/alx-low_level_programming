fef8e345332a7cd95fcf6ba984afd0e67f7e4e06
Favour <ofavourite001@gmail.com>
2023-04-13 21:08:07 +0000

    Create files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - returns a pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *favour;
+	favour = malloc(b);
+}
