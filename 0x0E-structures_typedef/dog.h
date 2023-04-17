35ded459fdf944812ee40ac0cc0c1cc0706e3d21
Favour <ofavourite001@gmail.com>
2023-04-17 20:47:11 +0000

    Make files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the specs of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: shows name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*MAIN_H*/
