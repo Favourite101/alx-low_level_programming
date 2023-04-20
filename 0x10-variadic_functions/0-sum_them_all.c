0ec79a54c79c7a8942886df6240c401f65b85520
Favour <ofavourite001@gmail.com>
2023-04-20 10:43:35 +0000

    Make files and add 0-

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all arguments
+ * @n: first argument
+ *
+ * Return: sum of integers
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list app;
+	int i = n, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(app, n);
+	while(i = va_arg(app, int))
+		sum += i;
+
+	return (sum);
+}
