616ac70c903e68966ddf7e866ad2a0e95c43b20c
Favour <ofavourite001@gmail.com>
2023-04-21 14:11:44 +0000

    Fixes

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,6 +14,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list app;
 
 	va_start(app, n);
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(app, char *);
