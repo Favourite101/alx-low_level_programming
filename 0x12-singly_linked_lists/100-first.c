873b97a0b2b14ff295edf6889ee5f69d57cc9949
Favour <ofavourite001@gmail.com>
2023-05-02 13:19:16 +0000

    Check 101-

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void print_text(void) __attribute__ ((constructor));
  */
 void print_text(void)
 {
-	printf("You're beat! and yet, you must allow, \n")
+	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
 }
