2134ebd88c2a7313f2488029231b4c9cc90017d0
Favour <ofavourite001@gmail.com>
2023-05-05 16:12:16 +0000

    Fix errors

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a decimal number in base 10
+ * @n: number to be printed
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	char *b;
+}
