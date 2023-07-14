5853f0073d02d0074fc2adfca5a38266d565a592
Favour <ofavourite001@gmail.com>
2023-07-14 19:18:22 +0000

    Add 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - returns a pointer
+ * @b: memory space to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *favour;
+
+	favour = malloc(b);
+	if (favour == NULL)
+		exit(98);
+
+	return (favour);
+}
