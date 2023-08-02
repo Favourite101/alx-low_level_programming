ca302028ebaca0a9c064b75bd4d65e32a9d4c520
Favour <ofavourite001@gmail.com>
2023-08-02 22:55:20 +0100

    Complete advanced tasks

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,15 +10,16 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *val = head;
-
+	listint_t *val;
+    
+    val = head;
 	while (val && i < index)
 	{
 		val = val->next;
 		i++;
 	}
-    if (!val)
+    if (val == NULL)
         return (NULL);
-
-	return (val);
+    else
+	    return (val);
 }
