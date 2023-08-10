e5f70c6910c2348e3beabaee668d2c096ac0d35a
Favour <ofavourite001@gmail.com>
2023-08-10 17:37:41 +0100

    Qucik fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * appen_text_to_file - append text to a file
+ * append_text_to_file - append text to a file
  * @filename: name of file
- * @content: content to append
+ * @text_content: content to append
  *
  * Return: 1 or -1
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int op, wr, len = 0;
+	int op, wr, len = 0;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content != NULL)
-        while (text_content[len])
-        {
-            len++;
-        }
-    op = open(filename, O_WRONLY | O_APPEND);
-    wr = write(op, text_content, len);
-    if (op == -1 || wr == -1)
-        return (-1);
-    close(op);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+	if (op == -1 || wr == -1)
+		return (-1);
+	close(op);
 
-    return (1);
+	return (1);
 }
