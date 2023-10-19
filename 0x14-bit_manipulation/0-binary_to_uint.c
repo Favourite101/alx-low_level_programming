21a3c54667209b0f2da577826bcb9afaf94e854d
Favourite101 <ofavourite001@gmail.com>
2023-10-19 19:56:46 +0000

    Changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,25 +8,25 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int res, pow;
+	int i;
+	unsigned int res, pow;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-        {
-            return (0);
-        }
-    }
-    for (pow = 1, res = 0, i--; i >= 0; i--, pow *= 2)
-    {
-        if (b[i] == '1')
-            res += pow;
-    }
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	for (pow = 1, res = 0, i--; i >= 0; i--, pow *= 2)
+	{
+		if (b[i] == '1')
+			res += pow;
+	}
 
-return (res);
+	return (res);
 }
