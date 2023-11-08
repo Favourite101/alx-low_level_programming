4e2a4d7da85469e4968045672b607c87f4385cbf
Favour <ofavourite001@gmail.com>
2023-11-08 14:00:13 +0100

    0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenates two strings
+ * @dest: pointer to be checked
+ * @src: pointer to be checked
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int test_1 = 0, test_2 = 0;
+
+	while (dest[test_1] != '\0')
+	{
+		test_1++;
+	}
+	while (src[test_2] != '\0')
+	{
+		dest[test_1] = src[test_2];
+		test_2++;
+		test_1++;
+	}
+	dest[test_1] = '\0';
+
+	return (dest);
+}
