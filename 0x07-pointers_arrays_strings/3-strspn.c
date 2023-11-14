84e9fc00e2f5d3a5573a70a792ba24e85e7a53d1
Favour <ofavourite001@gmail.com>
2023-11-14 14:49:39 +0100

    0x07 fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, flag, f = 0;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (j = 0; j != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
@@ -22,10 +22,10 @@ unsigned int _strspn(char *s, char *accept)
 				flag = 1;
 			}
 		}
-		//if (flag == 0)
-		//{
-		//	return (0);
-		//}
+		if (flag == 0)
+		{
+			return (0);
+		}
 	}
 	return (f);
 }
