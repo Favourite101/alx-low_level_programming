dbb2a8f04d42ced833e7564f4b4108497c1a682c
Favour <ofavourite001@gmail.com>
2023-11-15 10:13:00 +0100

    0x08

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial
+ * @n: number
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	int prod;
+
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	prod = n * factorial(n - 1);
+
+	return (prod);
+}
